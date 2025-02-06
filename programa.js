ab87de76a80777b2eced2b2ff9e8a398ed270be0
Laura Urrea <lauramilena.99.10@gmail.com>
2025-02-05 20:48:10 -0500

    V 0.3

--- programa.js
+++ programa.js
@@ -1,1 +1,7 @@
-hh
+var map = L.map('map').setView([4.570515893694413, -74.14399940047235], 13); 
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
